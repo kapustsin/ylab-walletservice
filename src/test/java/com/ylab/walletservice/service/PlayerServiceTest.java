37ea1609fe1328ef33063b95c9fdab1fb778bc4d
Kapustsin Aliaksandr <a.p.kapustsin@gmail.com>
2023-10-11 17:49:55 +0300

    add tests

---
+++ src/test/java/com/ylab/walletservice/service/PlayerServiceTest.java
@@ -0,0 +1,78 @@
+package com.ylab.walletservice.service;
+
+import com.ylab.walletservice.domain.Player;
+import com.ylab.walletservice.domain.repository.PlayerRepository;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class PlayerServiceTest {
+    private PlayerRepository playerRepository;
+    private PlayerService playerService;
+
+    @Before
+    public void setUp() {
+        playerRepository = mock(PlayerRepository.class);
+        playerService = new PlayerService(playerRepository);
+    }
+
+    @Test
+    public void testCreateNewUserSuccess() {
+        when(playerRepository.exists("newUser")).thenReturn(false);
+
+        boolean result = playerService.create("newUser", "password123");
+
+        assertTrue(result);
+        verify(playerRepository, times(1)).create("newUser", "password123");
+    }
+
+    @Test
+    public void testCreateExistingUser() {
+        when(playerRepository.exists("existingUser")).thenReturn(true);
+
+        boolean result = playerService.create("existingUser", "password123");
+
+        assertFalse(result);
+        verify(playerRepository, never()).create(anyString(), anyString());
+    }
+
+    @Test
+    public void testDoAuthorizationSuccess() {
+        Player existingPlayer = new Player(1, "user123", "password123");
+        when(playerRepository.get("user123")).thenReturn(existingPlayer);
+
+        Optional<Player> result = playerService.doAuthorisation("user123", "password123");
+
+        assertTrue(result.isPresent());
+        assertEquals(existingPlayer, result.get());
+    }
+
+    @Test
+    public void testDoAuthorizationFailure() {
+        when(playerRepository.get("nonExistingUser")).thenReturn(null);
+
+        Optional<Player> result = playerService.doAuthorisation("nonExistingUser", "wrongPassword");
+
+        assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void testGetBalance() {
+        Player player = new Player(1, "user123", "password123");
+        player.setBalance(1000.0);
+        when(playerRepository.get("user123")).thenReturn(player);
+
+        double balance = playerService.getBalance("user123");
+
+        assertEquals(1000.0, balance, 0.0);
+    }
+}
