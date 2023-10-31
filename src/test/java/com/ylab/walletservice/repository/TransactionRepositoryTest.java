f76f69c704129bb96688e080b2489716e7a08091
Kapustsin Aliaksandr <a.p.kapustsin@gmail.com>
2023-10-31 18:28:32 +0300

    fixed previous lesson's comments

--- src/test/java/com/ylab/walletservice/repository/TransactionRepositoryTest.java
+++ src/test/java/com/ylab/walletservice/repository/TransactionRepositoryTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
